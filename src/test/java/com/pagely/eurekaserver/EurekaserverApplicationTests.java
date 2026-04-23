3e3d58f7713eebc6b7f1ca338b26c9d04a8593eb
gnoesnooj <306yyy@naver.com>
2026-04-23 16:47:17 +0900

    Initial Setting

---
+++ src/test/java/com/pagely/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagely.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
